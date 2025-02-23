c6171b8146a699b61a1d4b4161d3328263ac8bf3
sofiamattinzioli <334739@edu.mkcollege.ac.uk>
2025-02-23 15:53:01 +0000

    Base layer - added identity account & ApplicationUser

---
+++ Task2_WebApp_2025/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Task2_WebApp_2025.Models;
+
+namespace Task2_WebApp_2025.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Task2_WebApp_2025/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Task2_WebApp_2025.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
